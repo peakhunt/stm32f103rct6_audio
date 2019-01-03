f83993154313913f823c0a9545aec3f7c5fe3450
Hawk Kim <hawk.kim1974@gmail.com>
2019-01-03 17:12:20 +0900

    added FFT processing skeleton

--- app/audio.c
+++ app/audio.c
@@ -1,22 +1,56 @@
 #include "audio.h"
 #include "arm_math.h"
+#include "arm_const_structs.h"
 
-static arm_fir_instance_f32   _fir_filter;
-static float32_t              _fir_coeffs[1] = { 0.00f };
-static float32_t              _fir_states[1] = { 0.00f };
+static arm_rfft_fast_instance_f32   _fft_f32;
+
+static float32_t                    _fft_in_buf[AUDIO_BUFFER_SIZE];
+static float32_t                    _fft_out_buf[AUDIO_BUFFER_SIZE];
 
 void
 audio_init(void)
 {
-  arm_fir_f32(
-      &_fir_filter,
-      _fir_coeffs,
-      _fir_states,
-      64);
+  arm_rfft_fast_init_f32(&_fft_f32, AUDIO_BUFFER_SIZE);
 }
 
 void
 audio_process(audio_buffer_t* b)
 {
-  (void)_fir_filter;
+  //////////////////////////////////////////////////
+  //
+  // XXX
+  //
+  // sample size: 128
+  // sampling frequency : 128 KHz
+  //
+  // then FFT frequency range
+  // 128KHz/2 * linspace(0,1, 128)
+  //
+  // 64 / 128 = roughly 0.5KHz per each BIN
+  //
+  // so mostly indices between 0 and 15 are of primary
+  // interest to me.
+  //
+  //////////////////////////////////////////////////
+
+  for(int i = 0; i < AUDIO_BUFFER_SIZE; i++)
+  {
+    _fft_in_buf[i] = b->buffer[i];
+  }
+
+  // time domain to frequency domain
+  arm_rfft_fast_f32(&_fft_f32, _fft_in_buf, _fft_out_buf, 0);
+
+  //
+  // FIXME
+  // add DSP processing
+  //
+
+  // frequency domain to time domain
+  arm_rfft_fast_f32(&_fft_f32, _fft_out_buf, _fft_in_buf, 1);
+
+  for(int i = 0; i < AUDIO_BUFFER_SIZE; i++)
+  {
+    b->buffer[i] = (uint16_t)(_fft_in_buf[i]);
+  }
 }
